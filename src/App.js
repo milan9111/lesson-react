8e0962d3c030cb44af3e1116311d7079831c587c
VolynetsPanda <hw.volynets@gmail.com>
2021-12-19 14:05:43 +0200

    test 4

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       HELLO
         <h1>Vasya</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, recusandae?</p>
     </div>
   );
 }
