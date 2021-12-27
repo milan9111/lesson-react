ffd9bc281a1895a279f1ef256abf656aac4b2a23
OleksiyTaranenko <oleksiytaranenko01@gmail.com>
2021-12-27 05:05:59 +0200

    created productsGallery block

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header';
+import ProductsGallery from './components/ProductsGallery';
 import Feedback from './components/Feedback/Feedback';
 import Footer from './components/Footer/Footer';
-  
+
 function App() {
   return (
     <div className="App">
       <Router>
-         <Routes>
-            <Route path="/" element={<Header />} />
-         </Routes>
-         <Feedback />
-         <Footer />
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
+        <ProductsGallery />
+        <Feedback />
+        <Footer />
       </Router>
     </div>
   );
